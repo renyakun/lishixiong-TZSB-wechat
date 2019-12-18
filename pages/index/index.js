02c69f1c0b0dd9d7849329e9a9a3fc9fdeed7b9d
lishixiong <1029449742@qq.com>
2019-12-18 18:41:01 +0800

    工作台页面优化 审核详情页初步

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,8 +9,62 @@ import {
   wxRequest
 } from '../../utils/WeChatfction';
 
+// echars
+import * as echarts from '../../my-component/ec-canvas/echarts.js';
+
+
 const app = getApp()
 const cookie = getApp().globalData.cookie;
+
+
+
+// 绘制图形,that获取当前实例传递进去
+function initChart(canvas, width, height, that) {
+  console.log(that.data.reportList);
+  let checkNum = that.data.reportList.checkNum;
+  let proveNum = that.data.reportList.proveNum;
+
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    backgroundColor: "#ffffff",
+    color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+      type: 'pie',
+      center: ['50%', '50%'],
+      radius: [0, '60%'],
+      data: [{
+        value: checkNum,
+        name: '待审核'
+      }, {
+        value: proveNum,
+        name: '待审批'
+      }],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 2, 2, 0.3)'
+        }
+      }
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+
+
+
 Page({
   data: {
     TabCur: 0,
@@ -19,16 +73,18 @@ Page({
       "待审核",
       "待审批"
     ],
-    reportList:[]
+    reportList: [],
+    ApproveList: [],
+    ec: {}
   },
 
   // 获取今日报告数量
   // GET /report/getUserNotifyInfo
-  getUserNotifyInfo(){
+  getUserNotifyInfo() {
     // wxRequest(method, url, data, cookie, callback, errFun )
     let cookie = getApp().globalData.cookie;
     console.log(cookie);
-    wxRequest('GET',url + '/report/getUserNotifyInfo', {}, cookie,(res)=>{
+    wxRequest('GET', url + '/report/getUserNotifyInfo', {}, cookie, (res) => {
       console.log(res.data.data)
       if (res.data.ok) {
         console.log("设置数据")
@@ -36,30 +92,63 @@ Page({
           reportList: res.data.data
         })
       }
-    },(err)=>{
+    }, (err) => {
       console.log(err)
     })
 
   },
 
   // 待审核报告列表
-  getWaitCheckList(){
+  getWaitCheckList() {
     let cookie = getApp().globalData.cookie;
     console.log(cookie);
     wxRequest('GET', url + '/report/getWaitCheckList', {
-      pageNum:1,
-      pageSize:10
+      pageNum: 1,
+      pageSize: 10
     }, cookie, (res) => {
       console.log(res.data.data)
       if (res.data.ok) {
         console.log("设置数据")
         this.setData({
-          checkList: res.data.data
+          checkList: res.data.data.list
         })
       }
     }, (err) => {
       console.log(err)
     })
+  },
+
+  // 待审批报告列表
+  getApproveList() {
+    let cookie = getApp().globalData.cookie;
+    console.log(cookie);
+    wxRequest('GET', url + '/report/getWaitApproveList', {
+      pageNum: 1,
+      pageSize: 10
+    }, cookie, (res) => {
+      console.log(res.data.data)
+      if (res.data.ok) {
+        console.log("设置数据")
+        this.setData({
+          ApproveList: res.data.data.list
+        })
+      }
+    }, (err) => {
+      console.log(err)
+    })
+  },
+
+  // 跳转到任务详情页
+  toReportInfo(e) {
+    console.log(e);
+    let reportNo = e.currentTarget.dataset.reportno;
+    console.log(reportNo);
+    setTimeout(() => {
+      wx.navigateTo({
+        url: '/pages/reportDetail/reportDetail?reportNo=' + reportNo,
+      })
+    }, 1000)
+
   },
 
   // tab栏切换
@@ -69,38 +158,18 @@ Page({
       scrollLeft: (e.currentTarget.dataset.id - 1) * 60
     })
   },
-  // ListTouch触摸开始
-  ListTouchStart(e) {
-    this.setData({
-      ListTouchStart: e.touches[0].pageX
-    })
+
+  // 初始化饼图
+  echartInit(e) {
+    let that = this;
+    initChart(e.detail.canvas, e.detail.width, e.detail.height, that);
   },
 
-  // ListTouch计算方向
-  ListTouchMove(e) {
-    this.setData({
-      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
-    })
-  },
-
-  // ListTouch计算滚动
-  ListTouchEnd(e) {
-    if (this.data.ListTouchDirection == 'left') {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    } else {
-      this.setData({
-        modalName: null
-      })
-    }
-    this.setData({
-      ListTouchDirection: null
-    })
-  },
   onLoad: function() {
     this.getUserNotifyInfo();
     this.getWaitCheckList();
+    this.getApproveList();
   },
+
 
 })
